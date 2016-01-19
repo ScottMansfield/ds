95319560eb78c245dcb1ce99c31e13f313bf705d
Scott Mansfield <sgmansf@gmail.com>
2016-01-19 14:55:29 -0800

    Adding sorted set skeleton

--- set/sortedset.go
+++ set/sortedset.go
@@ -22,3 +22,49 @@ func (s *SortedSet) Add(item interface{}) error {
     // lol
     return nil
 }
+
+func (s *SortedSet) AddAll(...interface{}) error {
+    // lol
+    return nil
+}
+
+func (s *SortedSet) Contains(interface{}) (bool, error) {
+    // lol
+    return false, nil
+}
+
+func (s *SortedSet) ContainsAll(...interface{}) (bool, error) {
+    // lol
+    return false, nil
+}
+
+func (s *SortedSet) Remove(interface{}) error {
+    // lol
+    return nil
+}
+
+func (s *SortedSet) RemoveAll(...interface{}) error {
+    // lol
+    return nil
+}
+
+func (s *SortedSet) Clear() error {
+    // lol
+    return nil
+}
+
+func (s *SortedSet) Size() (int, error) {
+    // lol
+    return -1, nil
+}
+
+func (s *SortedSet) IsEmpty() (bool, error) {
+    // lol
+    return false, nil
+}
+
+func (s *SortedSet) ToSlice() ([]interface{}, error) {
+    // lol
+    return nil, nil
+}
+
