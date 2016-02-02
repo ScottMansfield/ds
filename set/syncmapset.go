620d840686a1181f30d4241ce25c6dc23f7b4c8a
Scott Mansfield <sgmansf@gmail.com>
2016-02-01 23:42:59 -0800

    Set uses a reference to the mutex now so it can be copied around and not break

--- set/syncmapset.go
+++ set/syncmapset.go
@@ -1,109 +1,108 @@
 package set
 
 import (
-    "sync"
+	"sync"
 )
 
 type SyncMapSet struct {
-    lock sync.RWMutex
-    items map[interface{}]struct{}
+	lock  *sync.RWMutex
+	items map[interface{}]struct{}
 }
 
 func NewSyncMapSet() *SyncMapSet {
-    items := make(map[interface{}]struct{})
-
-    return &SyncMapSet{
-        items: items,
-    }
+	return &SyncMapSet{
+		lock:  new(sync.RWMutex),
+		items: make(map[interface{}]struct{}),
+	}
 }
 
 func (s *SyncMapSet) Add(item interface{}) error {
-    s.lock.Lock()
-    defer s.lock.Unlock()
+	s.lock.Lock()
+	defer s.lock.Unlock()
 
-    s.items[item] = struct{}{}
+	s.items[item] = struct{}{}
 
-    return nil
+	return nil
 }
 
 func (s *SyncMapSet) AddAll(items ...interface{}) error {
-    s.lock.Lock()
-    defer s.lock.Unlock()
+	s.lock.Lock()
+	defer s.lock.Unlock()
 
-    for _, item := range items {
-        s.items[item] = struct{}{}
-    }
-    
-    return nil
+	for _, item := range items {
+		s.items[item] = struct{}{}
+	}
+
+	return nil
 }
 
 func (s *SyncMapSet) Contains(item interface{}) (bool, error) {
-    s.lock.RLock()
-    defer s.lock.RUnlock()
+	s.lock.RLock()
+	defer s.lock.RUnlock()
 
-    _, ok := s.items[item]
+	_, ok := s.items[item]
 
-    return ok, nil
+	return ok, nil
 }
 
 func (s *SyncMapSet) ContainsAll(items ...interface{}) (bool, error) {
-    s.lock.RLock()
-    defer s.lock.RUnlock()
+	s.lock.RLock()
+	defer s.lock.RUnlock()
 
-    for _, item := range items {
-        if _, ok := s.items[item]; !ok {
-            return false, nil
-        }
-    }
+	for _, item := range items {
+		if _, ok := s.items[item]; !ok {
+			return false, nil
+		}
+	}
 
-    return true, nil
+	return true, nil
 }
 
 func (s *SyncMapSet) Remove(item interface{}) error {
-    s.lock.Lock()
-    defer s.lock.Unlock()
+	s.lock.Lock()
+	defer s.lock.Unlock()
 
-    delete(s.items, item)
+	delete(s.items, item)
 
-    return nil
+	return nil
 }
 
 func (s *SyncMapSet) RemoveAll(items ...interface{}) error {
-    s.lock.Lock()
-    defer s.lock.Unlock()
+	s.lock.Lock()
+	defer s.lock.Unlock()
 
-    for _, item := range items {
-        delete(s.items, item)
-    }
+	for _, item := range items {
+		delete(s.items, item)
+	}
 
-    return nil
+	return nil
 }
 
 func (s *SyncMapSet) Clear() error {
-    s.lock.Lock()
-    defer s.lock.Unlock()
+	s.lock.Lock()
+	defer s.lock.Unlock()
 
-    s.items = make(map[interface{}]struct{})
+	s.items = make(map[interface{}]struct{})
 
-    return nil
+	return nil
 }
 
 func (s *SyncMapSet) Size() (int, error) {
-    return len(s.items), nil
+	return len(s.items), nil
 }
 
 func (s *SyncMapSet) IsEmpty() (bool, error) {
-    return (len(s.items) == 0), nil
+	return (len(s.items) == 0), nil
 }
 
 func (s *SyncMapSet) ToSlice() ([]interface{}, error) {
-    s.lock.RLock()
-    defer s.lock.RUnlock()
+	s.lock.RLock()
+	defer s.lock.RUnlock()
 
-    ret := make([]interface{}, 0, len(s.items))
-    for item, _ := range s.items {
-        ret = append(ret, item)
-    }
+	ret := make([]interface{}, 0, len(s.items))
+	for item, _ := range s.items {
+		ret = append(ret, item)
+	}
 
-    return ret, nil
+	return ret, nil
 }
