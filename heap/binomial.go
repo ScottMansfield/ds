0f62f62eeb7c28331a75c1832d0930ce7237433c
Scott Mansfield <sgmansf@gmail.com>
2016-05-16 01:28:16 -0700

    just getting heap to build

--- heap/binomial.go
+++ heap/binomial.go
@@ -20,6 +20,18 @@ func NewBinomialMaxHeap() *BinomialMaxHeap {
 }
 
 func (b *BinomialMaxHeap) Add(key string, rank int, data interface{}) {
+	newNode := &binomialNode{
+		key:  key,
+		rank: rank,
+		data: data,
+	}
+
+	if b.root == nil {
+		b.root = newNode
+		return
+	}
+
+	// Adding a node is the same as making a new one-item heap and merging
 
 }
 
@@ -39,6 +51,43 @@ func (b *BinomialMaxHeap) PeekMax() *Val {
 	return nil
 }
 
-func (b *BinomialMaxHeap) Merge(h BinomialMaxHeap) *BinomialMaxHeap {
-	return nil
+// Merges BinomialHeap h into this heap,
+func (b *BinomialMaxHeap) Merge(h BinomialMaxHeap) {
+	// do nothing
 }
+
+func (bmh *BinomialMaxHeap) merge(b *binomialNode) {
+	a := bmh.root
+
+	// make a fake list head to make coding simpler
+	newList := &binomialNode{}
+	var extra *binomialNode
+
+	for a != nil && b != nil {
+		if extra != nil {
+			if extra.order > a.order && extra.order > b.order {
+				newList.next = extra
+				extra = nil
+			} else if extra.order == a.order {
+				if extra.order > b.order {
+
+				}
+			}
+		}
+		if a.rank < b.rank {
+			newList.next = a
+			a = a.next
+		} else if b.rank < a.rank {
+			newList.next = b
+			b = b.next
+		} else {
+			// Merge
+			// make smaller one the end of the child linked list
+		}
+	}
+
+	// chop off the fake head
+	newList = newList.next
+}
+
+//func mergeTrees
